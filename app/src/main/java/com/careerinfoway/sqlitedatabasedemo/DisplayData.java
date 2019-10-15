051da8a7ef7646b2fa8c851382dd2c40c0336596
ntmsolution <50775414+ntmsolution@users.noreply.github.com>
2019-10-15 17:31:43 +0530

    commit

--- app/src/main/java/com/careerinfoway/sqlitedatabasedemo/DisplayData.java
+++ app/src/main/java/com/careerinfoway/sqlitedatabasedemo/DisplayData.java
@@ -2,22 +2,29 @@ package com.careerinfoway.sqlitedatabasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayData extends AppCompatActivity {
 
     ListView lv;
     DBmain objdb;
     SQLiteDatabase db;
+    int[] id;
     String[] fname,lname,mobileno;
+    Button btnedit,btndelete;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +34,24 @@ public class DisplayData extends AppCompatActivity {
         lv = findViewById(R.id.lv);
         objdb = new DBmain(DisplayData.this);
 
-        db = objdb.getReadableDatabase();
-
         displaydata();
     }
 
     private void displaydata() {
 
+        db = objdb.getReadableDatabase();
+
         Cursor cursor = db.rawQuery("select * from student",null);
 
         if(cursor.getCount()>0){
+            id = new int[cursor.getCount()];
             fname = new String[cursor.getCount()];
             lname = new String[cursor.getCount()];
             mobileno = new String[cursor.getCount()];
 
             int i=0;
             while(cursor.moveToNext()){
+                id[i] = cursor.getInt(0);
                 fname[i] = cursor.getString(1);
                 lname[i] = cursor.getString(2);
                 mobileno[i] = cursor.getString(3);
@@ -53,9 +62,6 @@ public class DisplayData extends AppCompatActivity {
             Custom adapter = new Custom();
             lv.setAdapter(adapter);
         }
-
-
-
     }
 
     class Custom extends BaseAdapter{
@@ -76,16 +82,52 @@ public class DisplayData extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int i, View v, ViewGroup viewGroup) {
+        public View getView(final int i, View v, ViewGroup viewGroup) {
             v = LayoutInflater.from(DisplayData.this).inflate(R.layout.single_data,viewGroup,false);
 
             TextView tvfname = v.findViewById(R.id.fname);
             TextView tvlname = v.findViewById(R.id.lname);
             TextView tvmobileno = v.findViewById(R.id.mobileno);
+            btnedit = v.findViewById(R.id.btnedit);
+            btndelete = v.findViewById(R.id.btndelete);
 
             tvfname.setText(fname[i]);
             tvlname.setText(lname[i]);
             tvmobileno.setText(mobileno[i]);
+
+            btnedit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    String msg = "";
+                    msg += "Firstname = "+fname[i];
+                    msg += "Lastname = "+lname[i];
+                    msg += "MobileNo = "+mobileno[i];
+
+                    Bundle data = new Bundle();
+                    data.putInt("id",id[i]);
+                    data.putString("fname",fname[i]);
+                    data.putString("lname",lname[i]);
+                    data.putString("mobileno",mobileno[i]);
+                    Intent i = new Intent(DisplayData.this,MainActivity.class);
+                    i.putExtra("userdata",data);
+                    startActivity(i);
+                }
+            });
+
+            btndelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    db = objdb.getWritableDatabase();
+
+                    int recordid = db.delete("student","id="+id[i],null);
+
+                    if(recordid!=-1){
+                        Toast.makeText(DisplayData.this,"Record Deleted Successfully",Toast.LENGTH_SHORT).show();
+                        displaydata();
+                    }
+                }
+            });
 
             return v;
         }
--- app/src/main/java/com/careerinfoway/sqlitedatabasedemo/MainActivity.java
+++ app/src/main/java/com/careerinfoway/sqlitedatabasedemo/MainActivity.java
@@ -14,9 +14,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText edtfname,edtlname,edtmobileno;
-    Button btnsubmit,btndisplay;
+    Button btnsubmit,btndisplay,btnedit;
     DBmain objdb;
     SQLiteDatabase db;
+    int id=0;
 
 
     @Override
@@ -29,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
         edtmobileno = findViewById(R.id.edtmobileno);
         btnsubmit = findViewById(R.id.btnsubmit);
         btndisplay = findViewById(R.id.btndisplay);
+        btnedit = findViewById(R.id.btnedit);
+
+        if(getIntent().getBundleExtra("userdata")!=null){
+
+            Bundle bundle = getIntent().getBundleExtra("userdata");
+
+
+            id = bundle.getInt("id");
+            edtfname.setText(bundle.getString("fname"));
+            edtlname.setText(bundle.getString("lname"));
+            edtmobileno.setText(bundle.getString("mobileno"));
+
+
+            btnedit.setVisibility(View.VISIBLE);
+            btnsubmit.setVisibility(View.GONE);
+        }
 
 
         objdb = new DBmain(MainActivity.this);
@@ -61,6 +78,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,DisplayData.class));
+            }
+        });
+
+        btnedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues cv = new ContentValues();
+                cv.put("fname",edtfname.getText().toString());
+                cv.put("lname",edtlname.getText().toString());
+                cv.put("mobileno",edtmobileno.getText().toString());
+
+                db = objdb.getWritableDatabase();
+
+                long recid = db.update("student",cv,"id="+id,null);
+
+                if(recid !=-1){
+                    Toast.makeText(MainActivity.this,"Data Updated Successfully",Toast.LENGTH_SHORT).show();
+                    btnsubmit.setVisibility(View.VISIBLE);
+                    btnedit.setVisibility(View.GONE);
+
+                    edtfname.setText("");
+                    edtlname.setText("");
+                    edtmobileno.setText("");
+                }else{
+                    Toast.makeText(MainActivity.this,"Data Not Updated",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
